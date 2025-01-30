16307ffef08689d531b0cd783b89580cc7625e15
Gaurav Bhoi <92210859+Gaurav-Bhoi@users.noreply.github.com>
2025-01-30 09:43:42 +0530

    Voice support (#3)
    * voice support added
    
    * fix: bugs

--- src/Route/route.tsx
+++ src/Route/route.tsx
@@ -2,12 +2,30 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {TabInterface} from '../Interface/interface';
 import Home from '../Home Screen/Home';
 import Chatbot from '../ChatBot Screen/Chatbot';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator<TabInterface>();
 
 export const Tabs = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Home" component={Home} />
-    <Tab.Screen name="Chatbot" component={Chatbot} />
+    <Tab.Screen
+      name="Home"
+      component={Home}
+      options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: () => <AntDesign name="home" color="black" size={25} />,
+      }}
+    />
+    <Tab.Screen
+      name="Chatbot"
+      component={Chatbot}
+      options={{
+        tabBarLabel: 'Chatbot',
+        tabBarIcon: () => (
+          <MaterialCommunityIcons name={'robot'} size={25} color="black" />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
